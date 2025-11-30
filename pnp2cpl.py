943f6cfc42cc01d0644bb05371517507acc7ea18
Shi Jinghai <huaruhai@hotmail.com>
2025-11-30 14:05:53 +0800

    Update pnp2cpl.py

--- pnp2cpl.py
+++ pnp2cpl.py
@@ -1,20 +1,33 @@
 import os
 import re
 
-def convert_pnp_to_cpl(pnp_file):
+def convert_pnp_to_cpl(pnp_file, useMil=True, zh=True):
     cpl_file = pnp_file.replace('_pnp.xy', '_cpl.csv')
     print(f"Converting '{pnp_file}' to '{cpl_file}'...")
 
     with open(pnp_file, 'r', encoding='UTF-8') as infile, open(cpl_file, 'w', encoding='UTF-8') as outfile:
-        outfile.write("Variant: No variations\n")
-        outfile.write("Units used: mil\n")
-        outfile.write("\n")
-        outfile.write("Designator, Comment, Layer, Footprint, Center-X, Center-Y, Rotation, Description\n")
+        if zh:
+            outfile.write("变体: 没有变体\n")
+            if useMil:
+                outfile.write("长度单位：mil\n")
+            else:
+                outfile.write("长度单位：mm\n")
+            outfile.write("\n")
+            outfile.write("位号, 说明, 层, 规格, X中心座标, Y中心座标, 旋转度数\n")
+        else:
+            outfile.write("Variant: No variations\n")
+            if useMil:
+                outfile.write("Units used: mil\n")
+            else:
+                outfile.write("Units used: mm\n")
+            outfile.write("\n")
+            outfile.write("Designator, Comment, Layer, Package, Center-X, Center-Y, Rotation\n")
         for line in infile:
             # Skip lines that start with Via or Pad or comments
             if not line.strip().startswith('Via') and not line.strip().startswith('Pad') \
                and not re.match(r'^P[0-9]', line.strip()) \
-               and not line.strip().startswith('#') and not line.strip().startswith('Description:'):
+               and not line.strip().startswith('#') and not line.strip().startswith('Description:') \
+               and not line.strip().startswith('RefDes,Description,Package,X,Y,Rotation,Side,Mount'):
                 clean_line = line.replace('"', '')
                 for s in ['[SMD, multilayer]', '[SMD]', 'SandFlower', 'sandflower', '[SMD, electrolytic]']:
                     clean_line = clean_line.replace(s, '')
@@ -26,12 +39,21 @@ def convert_pnp_to_cpl(pnp_file):
                     footprint = parts[2].replace('[', '').replace(']', '').strip()
                     center_x = parts[3]
                     center_y = parts[4]
+                    if not useMil:
+                        center_x = str(round(float(center_x) * 25.4 / 1000, 4))
+                        center_y = str(round(float(center_y) * 25.4 / 1000, 4))
                     layer = parts[6]
                     # 替换Layer名称
                     if layer == "Top":
-                        layer = "TopLayer"
+                        if zh:
+                            layer = "顶层"
+                        else:
+                            layer = "TopLayer"
                     elif layer == "Bottom":
-                        layer = "BottomLayer"
+                        if zh:
+                            layer = "底层"
+                        else:
+                            layer = "BottomLayer"
                     rotation = parts[5]
                     outfile.write(f"{designator},{comment},{layer},{footprint},{center_x},{center_y},{rotation}\n")
                 else:
@@ -45,7 +67,7 @@ if __name__ == "__main__":
     found = False
     for filename in os.listdir('.'):
         if filename.endswith('_pnp.xy'):
-            convert_pnp_to_cpl(filename)
+            convert_pnp_to_cpl(filename, False)
             found = True
     if not found:
         print("No _pnp.xy file found in the current directory.")
